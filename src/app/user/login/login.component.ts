45cd936feb80d7147a34a877d5dd6e65d7ec0cde
Luka Ogadze <lukaogadze@live.com>
2022-12-05 20:19:50 +0400

    end of module 8

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    readonly loginForm = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        password: new FormControl('', [Validators.required]),
+    });
 
+    login() {
+        console.log(this.loginForm.value);
+    }
 }
